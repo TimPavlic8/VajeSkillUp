37ab73f04c905bad0f6569c3ae4dd7642ace18a5
TimPavlic98 <122461013+TimPavlic98@users.noreply.github.com>
2023-01-30 22:39:22 +0100

    JavaScript crash course vaje 1

---
+++ JavaScript-Crash-Course/main.js
@@ -0,0 +1,58 @@
+/* console.log("hello from main.js");
+
+//Primitive value
+const name = "Vishwas";
+const language = "JavaScript";
+const channel = "";
+
+const total = 0;
+const PI = 3.14;
+
+const isPrimaryNumber = true;
+isNewUser = false;
+
+let result;
+console.log(result);
+
+const res = null;
+
+const data = null;
+
+//Non-primitive value
+const person = {
+  firstName: "bruce",
+  lastName: "Wayne",
+  age: 30,
+};
+
+console.log(person.firstName);
+
+//Array
+const oddNumbers = [1, 3, 5, 7, 9];
+console.log(oddNumbers[0]);
+ */
+
+/* console.log(Number("5"));
+
+console.log(String(500));
+//or
+console.log((500).toString());
+
+console.log(Boolean(5)); // {null, undefined, 0, "", NaN} = "false"
+ */
+
+/* const color = "green";
+
+switch(color){
+    case 'red':
+            console.log("color is red");
+            break
+    case 'blue':
+            console.log("color is blue");
+    break
+    case 'green':
+            console.log("color is green");
+    break
+    default:
+            console.log("Not a valid color");
+} */
