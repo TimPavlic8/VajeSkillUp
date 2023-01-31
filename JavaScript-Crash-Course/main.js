11d3d8584dd13920cf1bf99788ee73d1fa275ece
TimPavlic98 <122461013+TimPavlic98@users.noreply.github.com>
2023-01-31 21:27:04 +0100

    JS crash course vaja2

--- JavaScript-Crash-Course/main.js
+++ JavaScript-Crash-Course/main.js
@@ -56,3 +56,78 @@ switch(color){
     default:
             console.log("Not a valid color");
 } */
+
+////////////////////////////////////////////////////////////////////////
+
+/* //FOR LOOP
+// for(initializer; condition; final-expression){//code}
+for (let i = 1; i <= 5; i++) {
+        console.log('log'  + i);
+} */
+
+//WHILE LOOP
+/* initializer
+while(condition){
+        // code to run
+        final-expression
+}
+*/
+/* let i = 1;
+while(i <= 5){
+        console.log('log' + i)
+        i++
+}
+ */
+
+//DO..WHILE LOOP
+/*initializer
+do{
+    // code to run 
+    final-expression
+} while (condition)
+*/
+/* let i = 1;
+do{
+        console.log('log' + i)
+        i++;
+}while(i <= 5); */
+
+//FOR..OF LOOP
+/* for(const item of array){
+        //code to run
+}*/
+/* const numArray = [1, 2, 3, 4, 5];
+
+for (const num of numArray){
+        console.log('log' + num);
+} */
+
+////////////////////////////////////////////////////////////////////////
+
+// F - U - N - C - T - I - O - N - S //
+
+/* 
+function name(parameter1, parameter2, parameter3){
+        //code to be executed
+}
+*/
+
+/* 
+function greet(username){
+        console.log("Hi! " + username);
+}
+
+function add(a, b){
+        return a + b;
+}
+
+const arrowSum = (a, b) => a + b;
+const addFive = (num) => num + 5;
+
+const sum1 = arrowSum(5, 6);
+
+const sum = addFive(5);
+
+console.log(sum1);
+console.log(sum); */
+
